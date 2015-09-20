01748aed467da1dfa141a73609305d81de4322b8
OscarCarranza <oscar.carranza@unitec.edu>
2015-09-20 13:59:06 -0600

    Agregando clase actor

---
+++ src/p4/hollywooduniverse/Actor.java
@@ -0,0 +1,49 @@
+
+package p4.hollywooduniverse;
+
+import hollywood.Movie;
+import java.util.ArrayList;
+public class Actor {
+    
+    protected String name;
+    protected String nacionality;
+    protected int age;
+    protected ArrayList <Movie> movies = new ArrayList();
+    
+    public Actor(String name, String nacionality, int age, ArrayList <Movie> movies){
+        this.name = name;
+        this.nacionality = nacionality;
+        this.age = age;
+        this.movies = movies;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getNacionality(){
+        return nacionality;
+    }
+    
+    public void setNacionality(String nacionality){
+        this.nacionality = nacionality;
+    }
+    
+    public int getAge(){
+        return age;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+    public void addMovie(Movie m){
+        movies.add(m);
+    }
+    
+    
+}
